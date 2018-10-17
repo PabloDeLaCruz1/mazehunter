a04435e657895e160e2053a133c297e3032780bc
Pablo De La Cruz <pdelac@ramapo.edu>
2018-10-17 11:03:45 -0400

    MVP draft

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
+  get "welcome/index"
+
+  get "/login", to: "sessions#new", as: "login" #login_path, loging_url
+  post "/login", to: "sessions#create"
+
+  get "/signup", to: "users#new", as: "signup"
+  post "/signup", to: "users#create"
+
+  delete "/logout", to: "sessions#destroy", as: "logout"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, except: [:new]
+  root "welcome#index"
+end
